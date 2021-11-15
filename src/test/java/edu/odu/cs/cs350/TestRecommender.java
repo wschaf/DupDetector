539760dc9b31de2045d65891218122f2578a2fc0
Will Schaffer <wschaffer5@gmail.com>
2021-11-15 04:55:46 +0000

    Created Recommender class

---
+++ src/test/java/edu/odu/cs/cs350/TestRecommender.java
@@ -0,0 +1,26 @@
+package edu.odu.cs.cs350;
+
+import edu.odu.cs.cs350.Mocks.*;
+
+import java.util.*;
+import org.junit.jupiter.api.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestRecommender {
+    @Test
+    void testGetRecommendations() {
+
+    }
+
+    @Test
+    void testGetTokens() {
+
+    }
+
+    @Test
+    void testSetTokens() {
+
+    }
+}
