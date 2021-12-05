db19142e39e320bb33bd193014cbda196c49c453
Christian Martinez <cmart075@odu.edu>
2021-12-05 18:26:51 -0500

    Included some Integration test

--- src/test/java/edu/odu/cs/cs350/IntergrationTest.java
+++ src/test/java/edu/odu/cs/cs350/IntergrationTest.java
@@ -2,6 +2,7 @@ package edu.odu.cs.cs350;
 
 import java.util.*;
 import java.io.*;
+import java.io.File;
 import org.junit.jupiter.api.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -9,15 +10,52 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class IntergrationTest {
     
-    private List<File> actual;
+    private RecursiveSearch search;
+    private Output out;
+    private static String directory;
+    private static String properties;
+    private static List<File> expectedFiles;
 
     @BeforeEach
-    public void setup() {
+    public void setup() throws Exception {
+        out = new Output();
+        search = new RecursiveSearch();
+        expectedFiles = new ArrayList<File>();
+        
+        directory = "src/";
+        properties = "properties.ini";
 
+        expectedFiles.add(new File("src/test/data/Point.cpp"));
+        expectedFiles.add(new File("src/test/data/Point.h"));
+        expectedFiles.add(new File("src/test/data/readingList.cpp"));
+        expectedFiles.add(new File("src/test/data/readingList.h"));
+        expectedFiles.add(new File("src/test/data/test.cpp"));
     }
 
     @Test
-    public void testSectionOneFunctionality() {
+    public void testSectionOneFunctionality() throws Exception {
+        String expected = 
+        "File Scanned:\n" + 
+        "    /home/runner/work/DupDetector/DupDetector/src/test/data/Point.cpp, 189\n" +
+        "    /home/runner/work/DupDetector/DupDetector/src/test/data/Point.h, 200\n" +
+        "    /home/runner/work/DupDetector/DupDetector/src/test/data/readingList.cpp, 720\n" +
+        "    /home/runner/work/DupDetector/DupDetector/src/test/data/readingList.h, 194\n" +
+        "    /home/runner/work/DupDetector/DupDetector/src/test/data/test.cpp, 24\n";
+
+        out.setFiles(search.searchDirectory(directory));
+        assertThat(out.getFiles(), equalTo(expectedFiles));
+        assertThat(out.getFiles().size(), is(5));
+        assertThat(out.getSectionOne(), equalTo(expected));
+
+        out.setFiles(search.searchWithProperties(directory, properties));
+        assertThat(out.getFiles(), equalTo(expectedFiles));
+        assertThat(out.getFiles().size(), is(5));
+        assertThat(out.getSectionOne(), equalTo(expected));
+    }
+
+    @Test
+    public void testSectionTwoFunctionality() throws Exception {
         
     }
+
 }
