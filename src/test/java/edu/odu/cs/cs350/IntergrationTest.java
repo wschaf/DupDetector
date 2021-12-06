818f71ca4c463f15f45fa98680a11dad3507ce46
Christian Martinez <cmart075@odu.edu>
2021-12-05 23:14:23 -0500

    fixed failed test

--- src/test/java/edu/odu/cs/cs350/IntergrationTest.java
+++ src/test/java/edu/odu/cs/cs350/IntergrationTest.java
@@ -11,14 +11,16 @@ import static org.junit.jupiter.api.Assertions.*;
 public class IntergrationTest {
     
     private RecursiveSearch search;
-    private Output out;
+    private Output out1;
+    private Output out2;
     private static String directory;
     private static String properties;
     private static List<File> expectedFiles;
 
     @BeforeEach
     public void setup() throws Exception {
-        out = new Output();
+        out1 = new Output();
+        out2 = new Output();
         search = new RecursiveSearch();
         expectedFiles = new ArrayList<File>();
         
@@ -42,15 +44,15 @@ public class IntergrationTest {
         "    /home/runner/work/DupDetector/DupDetector/src/test/data/test.cpp, 24\n" +
         "    /home/runner/work/DupDetector/DupDetector/src/test/data/readingList.h, 194\n";
 
-        out.setFiles(search.searchDirectory(directory));
-        assertTrue(out.getFiles().containsAll(expectedFiles));
-        assertThat(out.getFiles().size(), is(5));
-        assertThat(out.getSectionOne(), equalTo(expected));
+        out1.setFiles(search.searchDirectory(directory));
+        assertTrue(out1.getFiles().containsAll(expectedFiles));
+        assertThat(out1.getFiles().size(), is(5));
+        assertThat(out1.getSectionOne(), equalTo(expected));
 
-        out.setFiles(search.searchWithProperties(directory, properties));
-        assertTrue(out.getFiles().containsAll(expectedFiles));
-        assertThat(out.getFiles().size(), is(5));
-        assertThat(out.getSectionOne(), equalTo(expected));
+        out2.setFiles(search.searchWithProperties(directory, properties));
+        assertTrue(out2.getFiles().containsAll(expectedFiles));
+        assertThat(out2.getFiles().size(), is(5));
+        assertThat(out2.getSectionOne(), equalTo(expected));
     }
 
     @Test
