e475e035ae51dbd8b1eae7a47574523b3a114ad5
Christian Martinez <cmart075@odu.edu>
2021-12-05 23:46:43 -0500

    fixed failed test

--- src/test/java/edu/odu/cs/cs350/IntergrationTest.java
+++ src/test/java/edu/odu/cs/cs350/IntergrationTest.java
@@ -10,7 +10,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class IntergrationTest {
     
-    private RecursiveSearch search;
+    private RecursiveSearch search1;
+    private RecursiveSearch search2;
     private Output out1;
     private Output out2;
     private static String directory;
@@ -21,7 +22,8 @@ public class IntergrationTest {
     public void setup() throws Exception {
         out1 = new Output();
         out2 = new Output();
-        search = new RecursiveSearch();
+        search1 = new RecursiveSearch();
+        search2 = new RecursiveSearch();
         expectedFiles = new ArrayList<File>();
         
         directory = "src/";
@@ -43,13 +45,13 @@ public class IntergrationTest {
         "    /home/runner/work/DupDetector/DupDetector/src/test/data/Point.h, 200\n" +
         "    /home/runner/work/DupDetector/DupDetector/src/test/data/test.cpp, 24\n" +
         "    /home/runner/work/DupDetector/DupDetector/src/test/data/readingList.h, 194\n";
-        
-        out1.setFiles(search.searchDirectory(directory));
+
+        out1.setFiles(search1.searchDirectory(directory));
         assertTrue(out1.getFiles().containsAll(expectedFiles));
         assertThat(out1.getFiles().size(), is(5));
         assertThat(out1.getSectionOne(), equalTo(expected));
 
-        out2.setFiles(search.searchWithProperties(directory, properties));
+        out2.setFiles(search2.searchWithProperties(directory, properties));
         assertTrue(out2.getFiles().containsAll(expectedFiles));
         assertThat(out2.getSectionOne(), equalTo(expected));
     }
